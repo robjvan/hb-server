ffe11bb54cb655402052ced8ac93b1279b764097
Robert Vandelinder <robjvan@gmail.com>
2025-09-28 23:43:04 -0230

    Added API descriptions and examples

--- src/modules/haiku/entities/haiku.entity.ts
+++ src/modules/haiku/entities/haiku.entity.ts
@@ -14,27 +14,45 @@ export class Haiku {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ApiProperty({ description: 'First line of the haiku.' })
+  @ApiProperty({
+    description: 'First line of the haiku.',
+    example: 'I am a server',
+  })
   @Column('text')
   lineOne: string;
 
-  @ApiProperty({ description: 'Second line of the haiku.' })
+  @ApiProperty({
+    description: 'Second line of the haiku.',
+    example: 'My job is to serve data',
+  })
   @Column('text')
   lineTwo: string;
 
-  @ApiProperty({ description: 'Third line of the haiku.' })
+  @ApiProperty({
+    description: 'Third line of the haiku.',
+    example: 'I will do it well',
+  })
   @Column('text')
   lineThree: string;
 
-  @ApiProperty({ description: 'Input term used to generate the haiku.' })
+  @ApiProperty({
+    description: 'Input term used to generate the haiku.',
+    example: 'server',
+  })
   @Column('text')
   input: string;
 
-  @ApiProperty({ description: 'Creation date of the haiku.' })
+  @ApiProperty({
+    description: 'Creation date of the haiku.',
+    example: '2025-09-28T14:32:45.123Z',
+  })
   @Column({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
 
-  @ApiProperty({ description: 'Country of the user that generated the haiku.' })
+  @ApiProperty({
+    description: 'Country of the user that generated the haiku.',
+    type: Country,
+  })
   @ManyToOne(() => Country, (country) => country.haikus)
   @JoinColumn({ name: 'countryId' })
   country: Country;
--- src/modules/logging/logging.controller.ts
+++ src/modules/logging/logging.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { LoggingService } from './logging.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('logging')
 export class LoggingController {
   constructor(private readonly loggingService: LoggingService) {}
 
+  @ApiOperation({
+    // description: 'Retrieves all available log records',
+    summary: 'Retrieves all available log records',
+  })
   @Get()
   getAllLogs() {
     return this.loggingService.getAllLogs();
