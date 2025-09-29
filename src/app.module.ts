edc0f22ec25e24d7414f4f2af2291e19ae82d786
Robert Vandelinder <robjvan@gmail.com>
2025-09-28 23:19:51 -0230

    Added module and db imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { HaikuModule } from './modules/haiku/haiku.module';
+import { LoggingModule } from './modules/logging/logging.module';
+import { LocationModule } from './modules/location/location.module';
+import { OpenaiModule } from './modules/openai/openai.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    LoggingModule,
+    HaikuModule,
+    LocationModule,
+    OpenaiModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
