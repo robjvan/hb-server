5684c5384da6c1500a264a15a31087d029488580
Robert Vandelinder <robjvan@gmail.com>
2025-09-28 23:20:05 -0230

    Added global prefix, cors, validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 3000, () => {
+    // console.log(`Swagger API available at https://localhost:3000/api`);
+  });
 }
 bootstrap();
