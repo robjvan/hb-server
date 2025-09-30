e417903cf0fd176a91dfa4bca296ddc367dee99d
Robert Vandelinder <robjvan@gmail.com>
2025-09-30 19:13:22 -0230

    Removed unused files

--- src/modules/location/location.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { LocationService } from './location.service';
-
-@Controller('location')
-export class LocationController {
-  constructor(private readonly locationService: LocationService) {}
-}
