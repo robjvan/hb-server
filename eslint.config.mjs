39d2156106a3a68e61c1ee610097c91281c586c3
Robert Vandelinder <robjvan@gmail.com>
2025-09-30 20:06:28 -0230

    Tweaked linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,6 +33,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/require-await': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
     },
   },
 );
