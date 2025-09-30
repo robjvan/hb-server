108a06f543eeacc8f6b27e0287f4488e775051b9
Robert Vandelinder <robjvan@gmail.com>
2025-09-30 19:13:50 -0230

    Added LocationModule import

--- src/modules/haiku/haiku.module.ts
+++ src/modules/haiku/haiku.module.ts
@@ -4,9 +4,10 @@ import { HaikuController } from './haiku.controller';
 import { Haiku } from './entities/haiku.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoggingModule } from '../logging/logging.module';
+import { LocationModule } from '../location/location.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Haiku]), LoggingModule],
+  imports: [TypeOrmModule.forFeature([Haiku]), LoggingModule, LocationModule],
   controllers: [HaikuController],
   providers: [HaikuService],
 })
