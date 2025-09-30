a72e011035bcbc6c53248f29c62306481e31b8dc
Robert Vandelinder <robjvan@gmail.com>
2025-09-30 19:13:37 -0230

    Added HttpModule import

--- src/modules/location/location.module.ts
+++ src/modules/location/location.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LocationService } from './location.service';
-import { LocationController } from './location.controller';
 import { Country } from './entities/country.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoggingModule } from '../logging/logging.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Country]), LoggingModule],
-  controllers: [LocationController],
+  imports: [TypeOrmModule.forFeature([Country]), LoggingModule, HttpModule],
   providers: [LocationService],
+  exports: [LocationService],
 })
 export class LocationModule {}
